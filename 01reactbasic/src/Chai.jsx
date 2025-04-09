4bc52c77738d75c44c873fba69a22894783953cc
priyatosh roy <priyatoshroy121@gmail.com>
2025-04-10 01:32:22 +0530

    second commit

---
+++ 01reactbasic/src/Chai.jsx
@@ -0,0 +1,12 @@
+function Chai()
+{
+    return(
+        <>
+        <h3> chai k saath react sikhte hai</h3>
+        <h1>chai </h1>
+        <p> React bolta hai tum mutiple element nehi return kar sakte ek hi kar sakte ho lekin tum ek hi element k andar multiple element rakh sakte ho tu div bhi use kar sakte ho lekin kyu itna jhanjhat karna jab tum fragments hi use kar payega</p>
+        </>
+    )
+}
+
+export default Chai
